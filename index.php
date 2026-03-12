46a4d5ce63365ac2f59a8ce59ee66639a2f0c25b
rohamafatimajee-eng <rohamafatimajee@gmail.com>
2026-03-12 22:19:19 +0500

    qasim

--- index.php
+++ index.php
@@ -207,7 +207,7 @@ if ($action) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pro Meal Tracker</title>
+    <title>Moucho Pro Meal Tracker</title>
     <!-- Bootstrap 5 + Font Awesome -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
